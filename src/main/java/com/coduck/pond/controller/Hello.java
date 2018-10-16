8a12972f1a832870cda8d1bdc42348c8b2c41f75
PaDuckk <kang10019@naver.com>
2018-10-16 13:45:53 +0900

    Fixed git ignore file

---
+++ src/main/java/com/coduck/pond/controller/Hello.java
@@ -0,0 +1,5 @@
+package com.coduck.pond.controller;
+
+public class Hello {
+
+}
--- src/main/java/com/coduck/pond/controller/HomeController.java
+++ src/main/java/com/coduck/pond/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {//ㅎㅇㅎㅇ//zz//zzs
+	public String home(Model model) { //zz
 		return "home";
 	}
 }
