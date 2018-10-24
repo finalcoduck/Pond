ddaf43ef018d36652b606f2dc3213becc559dd2e
soulstrk <soulstrk1234@gmail.com>
2018-10-24 20:33:27 +0900

    파일 다운로드 추가

--- src/main/java/com/coduck/pond/core/utils/file/FileUtils.java
+++ src/main/java/com/coduck/pond/core/utils/file/FileUtils.java
@@ -4,7 +4,9 @@ import java.io.File;
 
 public class FileUtils {
 	
-	//실제 파일 경로로 삭제
+	/*
+	 * 실제 파일 경로 삭제
+	 */
 	public static boolean deleteFile(String filePath) {
 		File ff = new File(filePath);
 		if(!ff.exists()) {
@@ -13,16 +15,21 @@ public class FileUtils {
 		return ff.delete();
 	}
 	
-	//파일 사이즈
+	/*
+	 * 파일 사이즈
+	 */
 	public static long changeFileSize(long fileSize) {
 		double newSize = fileSize/1024.0;
 		fileSize = (long)Math.ceil(newSize);
 		return fileSize;
 	}
 	
-	//파일 확장자
+	/*
+	 * 파일 확장자
+	 */
 	public static String getFileType(String fileName) {
 		fileName = fileName.trim().substring(fileName.length()-3).toLowerCase();
 		return fileName;
 	}
+	
 }
